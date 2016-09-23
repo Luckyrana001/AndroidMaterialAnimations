e91d37c1258ef227e360c153c12e13e2fbbee780
Lucky Rana <luckyrana321@gmail.com>
2016-09-23 14:28:02 +0800

    anim sequence and profile pic anim added

--- app/src/main/java/com/example/ranalucky/androidmaterialanimations/MainActivity.java
+++ app/src/main/java/com/example/ranalucky/androidmaterialanimations/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.ranalucky.androidmaterialanimations;
 import android.animation.Animator;
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.OvershootInterpolator;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -16,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     View container;
     boolean playAnimations = true;
 
-    private View welcome,profilePic,loginBtn;
+    private View welcome,profilePic,loginBtn ;EditText emailEt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,8 +68,17 @@ public class MainActivity extends AppCompatActivity {
         float endWelcome = welcome.getX();
         ObjectAnimator welcomeAnimator = ObjectAnimator.ofFloat(welcome,View.X , startWelcomeX,endWelcome);
         welcomeAnimator.setDuration(1000);
-        welcome.setVisibility(View.VISIBLE);
-        welcomeAnimator.start();
+
+        /*a listener to set welcome text visible only after animation start*/
+        welcomeAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
+                welcome.setVisibility(View.VISIBLE);
+
+            }
+        });
+       // welcomeAnimator.start();
 
 
         /*profile pic animation*/
@@ -74,22 +87,50 @@ public class MainActivity extends AppCompatActivity {
 
         ObjectAnimator animateProfilePic = ObjectAnimator.ofPropertyValuesHolder(profilePic, scaleXholder,scaleYholder);
         animateProfilePic.setDuration(1000);
-        animateProfilePic.start();
+        //animateProfilePic.start();
 
         /*login button animation*/
         ObjectAnimator loginBtnAnimator = (ObjectAnimator) AnimatorInflater.loadAnimator(this,R.animator.sign_in_anim);
         loginBtnAnimator.setTarget(loginBtn);
-        loginBtnAnimator.start();
+       // loginBtnAnimator.start();
+
+
+        /*this part will handle the sequencing of animations*/
+        AnimatorSet set = new AnimatorSet();
+        set.play(welcomeAnimator).after(animateProfilePic);
+        set.play(welcomeAnimator).before(loginBtnAnimator);
+        set.start();
 
     }
 
     private void setLayout() {
         welcome = findViewById(R.id.welecomeLabel);
-
         loginBtn = findViewById(R.id.loginBtn);
-
+        emailEt = (EditText)findViewById(R.id.emailEt);
         container = findViewById(R.id.container);
 
         profilePic = findViewById(R.id.profilePic);
+
+        emailEt.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus && emailEt.getText().toString().equals("abc"))
+                {
+                    changeProfilePic();
+                }
+            }
+        });
+
+    }
+
+    private void changeProfilePic() {
+        profilePic.animate().rotationY(90).setDuration(500).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                ((ImageView)profilePic).setImageResource(R.drawable.ic_launcher);
+                profilePic.animate().rotationY(0).setDuration(500).setInterpolator(new OvershootInterpolator());
+            }
+        });
     }
 }
