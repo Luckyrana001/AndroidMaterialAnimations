59f633e984b96ed1664e2a02efa89e9a4574cfde
Lucky Rana <luckyrana321@gmail.com>
2016-10-06 15:40:38 +0800

    Reenter anim added

--- app/src/main/java/com/example/ranalucky/androidmaterialanimations/MainActivity.java
+++ app/src/main/java/com/example/ranalucky/androidmaterialanimations/MainActivity.java
@@ -10,14 +10,13 @@ import android.app.ActivityOptions;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Slide;
 import android.util.Pair;
+import android.view.Gravity;
 import android.view.View;
 import android.view.animation.OvershootInterpolator;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     View container;
@@ -29,9 +28,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+/*
+        this method add the common enter / exit transition to activity
+*/
+        setupWindowAnimations();
         setLayout();
 
 
+    }
+
+    private void setupWindowAnimations() {
+        // Re-enter transition is executed when returning to this activity
+        Slide slideTransition = new Slide();
+        slideTransition.setSlideEdge(Gravity.LEFT);
+        slideTransition.setDuration(500);
+        getWindow().setReenterTransition(slideTransition);
+        getWindow().setExitTransition(slideTransition);
     }
     private void setLayout() {
         welcome = findViewById(R.id.welecomeLabel);
@@ -107,8 +119,17 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void ShowOtherItemsAnimation() {
+
+        /*startWelcomeX is the staring point of animation */
         float startWelcomeX = 0 - welcome.getWidth();
+        /*x coordinate is the end point of aniation because this animation we are starting from minus X */
         float endWelcome = welcome.getX();
+
+       /* ObjectAnimator is a subclass of ValueAnimator provides support for animating properties on target objects.
+                The constructors of this class take parameters to define the target object that will be animated as well
+                as the name of the property that will be animated. Appropriate set/get functions are then determined
+        internally and the animation will call these functions as necessary to animate the property.*/
+
         ObjectAnimator welcomeAnimator = ObjectAnimator.ofFloat(welcome,View.X , startWelcomeX,endWelcome);
         welcomeAnimator.setDuration(1000);
 
@@ -125,6 +146,11 @@ public class MainActivity extends AppCompatActivity {
 
 
         /*profile pic animation*/
+
+        /*PropertyValuesHolder class holds information about a property and the values that  property should take on during an animation.
+        PropertyValuesHolder objects can be used to create animations with ValueAnimator or
+        ObjectAnimator that operate on several different properties in parallel.*/
+
         PropertyValuesHolder scaleXholder = PropertyValuesHolder.ofFloat(View.SCALE_X,1f);
         PropertyValuesHolder scaleYholder = PropertyValuesHolder.ofFloat(View.SCALE_Y,1f);
 
@@ -139,6 +165,13 @@ public class MainActivity extends AppCompatActivity {
 
 
         /*this part will handle the sequencing of animations*/
+
+        /*AnimatorSet class plays a set of Animator objects in the specified order. Animations can be set up to play together, in sequence, or after a specified delay.
+        There are two different approaches to adding animations to a AnimatorSet: either the playTogether() or playSequentially() methods
+        can be called to add a set of animations all at once, or the play(Animator) can be used in conjunction with methods in the Builder class to add animations one by one.
+       */
+
+
         AnimatorSet set = new AnimatorSet();
         set.play(welcomeAnimator).after(animateProfilePic);
         set.play(welcomeAnimator).before(loginBtnAnimator);
@@ -148,11 +181,15 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void changeProfilePic() {
+
+        /*Animate profile pic flip animation*/
         profilePic.animate().rotationY(90).setDuration(500).setListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animation) {
                 super.onAnimationEnd(animation);
                 ((ImageView)profilePic).setImageResource(R.drawable.ic_launcher);
+
+                /*OvershootInterpolator is an interpolator whose change flings forward and overshoots the last value then comes back.*/
                 profilePic.animate().rotationY(0).setDuration(500).setInterpolator(new OvershootInterpolator());
             }
         });
